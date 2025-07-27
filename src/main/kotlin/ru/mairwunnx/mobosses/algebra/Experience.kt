d57245dc60917a9d131c510aba83db2f59d77652
Pavel Erokhin <mairwunnx@gmail.com>
2025-07-27 18:53:55 +0300

    Добавил новый файл Experience.kt для расчета опыта в зависимости от уровня и класса босса с учетом различных факторов.

---
+++ src/main/kotlin/ru/mairwunnx/mobosses/algebra/Experience.kt
@@ -0,0 +1,29 @@
+package ru.mairwunnx.mobosses.algebra
+
+import org.bukkit.attribute.Attribute
+import ru.mairwunnx.mobosses.models.GeneralConfigurationModel.ExpGain
+import ru.mairwunnx.mobosses.models.GeneralConfigurationModel.FormulaSection
+import ru.mobosses.entities.BossEntity
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+/**
+ *   maxScale (L)  – потолок кэфа опыта по уровню
+ *   power    (p)  – влияет на крутизну
+ *   offset   (K)  – смещает точку перегиба
+ */
+
+context (preset: FormulaSection) fun experience(boss: BossEntity, baseExp: Int): Int {
+  val level = boss.level.toDouble()
+  val levelScale = 1.0 + (preset.maxScale - 1.0) * level.pow(preset.power) / (preset.offset + level.pow(preset.power))
+
+  val classMul = boss.clazz.experience
+  return (baseExp * classMul * levelScale).roundToInt()
+}
+
+context(preset: ExpGain) fun innerexperience(boss: BossEntity): Long {
+  val hpPart    = boss.entity.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!.baseValue * preset.healthFactor
+  val lvlPart   = preset.levelFactor * boss.level.toDouble().pow(preset.levelPower)
+  val rarity    = if (preset.rarityMul) boss.clazz.experience else 1.0
+  return ((hpPart + lvlPart) * rarity).toLong()
+}
