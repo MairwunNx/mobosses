57e85ca3e8b581c3f2c9d213b6180ec248ebc416
Pavel Erokhin <mairwunnx@gmail.com>
2025-07-27 18:55:59 +0300

    Добавил новый файл IntRangeSpec.kt для определения диапазона целых чисел с возможностью выборки случайного значения из этого диапазона.

---
+++ src/main/kotlin/ru/mairwunnx/mobosses/models/types/IntRangeSpec.kt
@@ -0,0 +1,10 @@
+package ru.mairwunnx.mobosses.models.types
+
+import kotlinx.serialization.Serializable
+import ru.mairwunnx.mobosses.serializers.IntRangeSpecSerializer
+import kotlin.random.Random
+
+@Serializable(with = IntRangeSpecSerializer::class) data class IntRangeSpec(val min: Int, val max: Int) {
+  fun sample(rng: Random = Random): Int = if (min == max) min else rng.nextInt(min, max + 1)
+  override fun toString() = if (min == max) "$min" else "$min-$max"
+}
