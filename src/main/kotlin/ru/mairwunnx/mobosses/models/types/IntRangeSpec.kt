15cb9e045b776051235003bd2b8f1ad79df23e07
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-02 23:36:50 +0300

    Изменил класс IntRangeSpec на обычный класс для улучшения структуры и соответствия стандартам Kotlin.

--- src/main/kotlin/ru/mairwunnx/mobosses/models/types/IntRangeSpec.kt
+++ src/main/kotlin/ru/mairwunnx/mobosses/models/types/IntRangeSpec.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 import ru.mairwunnx.mobosses.serializers.IntRangeSpecSerializer
 import kotlin.random.Random
 
-@Serializable(with = IntRangeSpecSerializer::class) data class IntRangeSpec(val min: Int, val max: Int) {
+@Serializable(with = IntRangeSpecSerializer::class) class IntRangeSpec(val min: Int, val max: Int) {
   fun sample(rng: Random = Random): Int = if (min == max) min else rng.nextInt(min, max + 1)
   override fun toString() = if (min == max) "$min" else "$min-$max"
 }
