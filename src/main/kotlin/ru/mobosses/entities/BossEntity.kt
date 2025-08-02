92cad261ec0c550df6b59ee5c5d82600ad4d93ad
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-02 23:37:08 +0300

    Добавил поддержку иерархии боссов в класс BossEntity, включая управление дочерними боссами и их уровнями. Улучшил отображение имени босса с учетом уровня.

--- src/main/kotlin/ru/mobosses/entities/BossEntity.kt
+++ src/main/kotlin/ru/mobosses/entities/BossEntity.kt
@@ -5,8 +5,22 @@ import ru.mairwunnx.mobosses.models.BossesConfigurationModel.BossConfig
 import ru.mairwunnx.mobosses.models.ClassesConfigurationModel.BossClass
 import java.util.UUID
 
-class BossEntity(val entity: LivingEntity, val bossType: BossConfig, val clazz: BossClass, var level: Int) {
+class BossEntity(
+  val entity: LivingEntity,
+  val bossType: BossConfig,
+  val clazz: BossClass,
+  var level: Int,
+  val parentBoss: UUID? = null,
+  val hierarchyDepth: Int = 0
+) {
   val uuid: UUID = entity.uniqueId
-  val displayName: String get() = "${bossType.name} ${color}[Lv. ${level}]"
   val color = clazz.color
+  val displayName: String get() = "${bossType.name} ${color}[Lv. $level]"
+
+  val childBosses: MutableSet<UUID> = mutableSetOf()
+
+  fun addChildBoss(child: UUID) { childBosses += child }
+  fun removeChildBoss(child: UUID) { childBosses -= child }
+
+  fun canHaveMoreChildBosses(maxAllowed: Int) = childBosses.size < maxAllowed
 }
